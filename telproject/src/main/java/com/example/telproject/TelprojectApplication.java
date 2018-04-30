4a324b032b555c6c6d8d1ae3b3a3e9d41fff1629
kostasevan <sdi1400042@di.uoa.gr>
2018-04-30 11:18:19 +0300

    Tel project demo v1

---
+++ telproject/src/main/java/com/example/telproject/TelprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.telproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelprojectApplication.class, args);
+    }
+}
---
+++ telproject/src/test/java/com/example/telproject/TelprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.telproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TelprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
