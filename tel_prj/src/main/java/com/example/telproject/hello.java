4153e69770f93800c89248b3baf2ac494e036677
PekosV <pekosvagg@gmail.com>
2018-05-09 14:01:45 +0300

    hello

--- tel_prj/src/main/java/com/example/telproject/hello.java
+++ tel_prj/src/main/java/com/example/telproject/hello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class hello{
+public class .hello{
 
     @RequestMapping("/hello")
     public String sayHello(@RequestParam(value = "name") String name) {
---
+++ telproject/src/main/java/com/example/telproject/HEllo.java
@@ -0,0 +1,4 @@
+package main.java.com.example.telproject;
+
+public class HEllo {
+}
