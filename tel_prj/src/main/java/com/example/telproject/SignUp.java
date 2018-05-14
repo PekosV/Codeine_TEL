ac8d936294aed9bdae67861ba10f59cf3776ace7
MARINOSAG <sdi1400002@di.uoa.gr>
2018-05-14 21:30:14 +0300

    yolo commit

--- tel_prj/src/main/java/com/example/telproject/SignUp.java
+++ tel_prj/src/main/java/com/example/telproject/SignUp.java
@@ -1,11 +1,7 @@
 package com.example.telproject;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +15,7 @@ public class SignUp {
     @RequestMapping(value= "/signup" ,method = RequestMethod.GET)
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "signup.html";
+        return "signup";
     }
 
 
--- tel_prj/src/main/java/com/example/telproject/home.java
+++ tel_prj/src/main/java/com/example/telproject/home.java
@@ -1,17 +1,18 @@
 package com.example.telproject;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class home {
-
-    @RequestMapping(value = "/home", method = RequestMethod.GET, produces = {"application/json"})
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = {"application/json"})
     @ResponseStatus(HttpStatus.OK)
     public Map<String, String> home_controller(String name, Model model) {
         model.addAttribute("name", name);
@@ -21,5 +22,4 @@ public class home {
         map.put("bill", "kwlogayros");
         return map;
     }
-
 }
