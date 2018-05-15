c1d252eee71c8b65a95f9834c95d4a1c273b677f
PekosV <pekosvagg@gmail.com>
2018-05-15 16:20:03 +0300

    login tests

--- tel_prj/src/main/java/com/example/telproject/LoginController.java
+++ tel_prj/src/main/java/com/example/telproject/LoginController.java
@@ -1,5 +1,8 @@
 package com.example.telproject;
 
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +15,34 @@ import java.util.Map;
 public class LoginController {
 
 
-    @RequestMapping(value="/login")
-    public String getLoginForm2() {
-        //System.out.println(allParams);
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public String mypage(@RequestParam Map<String , String> allParams,
+                                                    @AuthenticationPrincipal final UserDetails userDetails,
+                                                    Model model)
+    {
+
+        System.out.println(allParams);
+        System.out.println(allParams.get("error"));
+
+        if(userDetails!=null && allParams.get("logout")==null ){
+            System.out.println(userDetails);
+            return "redirect:/";
+        }
+
         return "login";
     }
 
+
+    @RequestMapping(value = "/", method = RequestMethod.GET )
+    public String login(Model model ,
+                        @AuthenticationPrincipal final UserDetails userDetails){
+
+        if(userDetails.getAuthorities().toString().contains("USER")){
+
+            return "redirect:/";
+
+
+    }
 
 
 }
